c7e0f22cb3a08bf9d41dabbd32235d5e30b0961a
Vitalii Dubovyk <vitaliy.dubovyk.1@gmail.com>
2023-04-23 19:41:33 +0300

    added exceptions

---
+++ src/main/java/com/practice/basejava/exception/ExistStorageException.java
@@ -0,0 +1,8 @@
+package com.practice.basejava.exception;
+
+public class ExistStorageException extends StorageException {
+
+  public ExistStorageException(String uuid) {
+    super("Resume " + uuid + " already exist", uuid);
+  }
+}
